b799478b0a12a709b46233c75dd30d21e4cc3644
KD76V <kvd5006@gmail.com>
2026-01-09 12:24:26 +0530

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function likePost(btn){
-  if(btn.classList.contains("liked")){
-    btn.innerText = "❤️ Like";
-    btn.classList.remove("liked");
-  }else{
-    btn.innerText = "💔 Liked";
-    btn.classList.add("liked");
-  }
-}
