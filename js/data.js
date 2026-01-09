746907611be8a0fc5a23c2b6991fefc0e908389d
KD76V <kvd5006@gmail.com>
2026-01-09 11:58:44 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,17 @@
+const POSTS = [
+  {
+    username: "alex_king",
+    image: "https://picsum.photos/600/400?random=1",
+    caption: "Freedom over everything."
+  },
+  {
+    username: "nightowl",
+    image: "https://picsum.photos/600/400?random=2",
+    caption: "Rules are made to be questioned."
+  },
+  {
+    username: "president",
+    image: "https://picsum.photos/600/400?random=3",
+    caption: "Power is mindset."
+  }
+];
